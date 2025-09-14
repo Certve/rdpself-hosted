8a001ef5c8f029548eb58900a9f6a55190e1bd5d
Ivasl <ivanssl@certveis.space>
2025-09-14 09:28:10 +0000

    update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,4 +1,4 @@
-# Add user & setup pinggy
+# Add user & setup Pinggy
 sudo useradd -m $(jq -r '.inputs.username' $GITHUB_EVENT_PATH)
 sudo adduser $(jq -r '.inputs.username' $GITHUB_EVENT_PATH) sudo
 echo $(jq -r '.inputs.username' $GITHUB_EVENT_PATH):$(jq -r '.inputs.password' $GITHUB_EVENT_PATH) | sudo chpasswd
@@ -7,8 +7,3 @@ sudo hostname $(jq -r '.inputs.computername' $GITHUB_EVENT_PATH)
 wget https://s3.ap-south-1.amazonaws.com/public.pinggy.binaries/cli/v0.2.5/linux/amd64/pinggy
 chmod +x ./pinggy
 echo -e "$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)\n$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)" | sudo passwd "$USER"
-rm -f .pinggy.log
-./pinggy --remote-management $(jq -r '.inputs.authtoken' $GITHUB_EVENT_PATH)
-./pinggy tcp 22 --log ".pinggy.log" &
-sleep 10
-echo "Pinggy Setup Completeed"
