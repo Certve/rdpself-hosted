92ca77725e65409bec27e5473a9933bb89e4fe48
Ivasl <ivanssl@certveis.space>
2025-09-20 06:44:09 +0000

    update Setup0.sh

--- Setup0.sh
+++ Setup0.sh
@@ -1,15 +1,14 @@
-# Add user & setup ngrok
+# Add user & setup pinggy
 sudo useradd -m $(jq -r '.inputs.username' $GITHUB_EVENT_PATH)
 sudo adduser $(jq -r '.inputs.username' $GITHUB_EVENT_PATH) sudo
 echo $(jq -r '.inputs.username' $GITHUB_EVENT_PATH):$(jq -r '.inputs.password' $GITHUB_EVENT_PATH) | sudo chpasswd
 sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo hostname $(jq -r '.inputs.computername' $GITHUB_EVENT_PATH)
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-chmod +x ./ngrok
+wget https://s3.ap-south-1.amazonaws.com/public.pinggy.binaries/cli/v0.2.5/linux/amd64/pinggy
+chmod +x ./pinggy
 echo -e "$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)\n$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)" | sudo passwd "$USER"
-rm -f .ngrok.log
-./ngrok authtoken $(jq -r '.inputs.authtoken' $GITHUB_EVENT_PATH)
-./ngrok tcp 22 --log ".ngrok.log" &
+rm -f .pinggy.log
+#./pinggy authtoken $(jq -r '.inputs.authtoken' $GITHUB_EVENT_PATH)
+./pinggy tcp 22 --log ".pinggy.log" &
 sleep 10
 echo "Ngrok Setup Completeed"
