a50f48634fe73f463ac066eb60645858d354026c
Ivasl <ivanssl@certveis.space>
2025-09-20 06:52:17 +0000

    update Setup0.sh

--- Setup0.sh
+++ Setup0.sh
@@ -4,11 +4,11 @@ sudo adduser $(jq -r '.inputs.username' $GITHUB_EVENT_PATH) sudo
 echo $(jq -r '.inputs.username' $GITHUB_EVENT_PATH):$(jq -r '.inputs.password' $GITHUB_EVENT_PATH) | sudo chpasswd
 sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo hostname $(jq -r '.inputs.computername' $GITHUB_EVENT_PATH)
-wget https://s3.ap-south-1.amazonaws.com/public.pinggy.binaries/cli/v0.2.5/linux/amd64/pinggy
-chmod +x ./pinggy
+#wget https://s3.ap-south-1.amazonaws.com/public.pinggy.binaries/cli/v0.2.5/linux/amd64/pinggy
+#chmod +x ./pinggy
 echo -e "$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)\n$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)" | sudo passwd "$USER"
-rm -f .pinggy.log
+#rm -f .pinggy.log
 #./pinggy authtoken $(jq -r '.inputs.authtoken' $GITHUB_EVENT_PATH)
-./pinggy tcp 22 --log ".pinggy.log" &
+#./pinggy tcp 22 --log ".pinggy.log" &
 sleep 10
-echo "Ngrok Setup Completeed"
+#echo "Ngrok Setup Completeed"
